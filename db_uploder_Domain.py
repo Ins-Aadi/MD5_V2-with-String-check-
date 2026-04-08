ea6940263252b146c5ff07bc5177ab46376773a1
Aditya Sharma <77775232+Ins-Aadi@users.noreply.github.com>
2026-04-08 21:45:55 +0530

    Update db_uploder_Domain.py

--- db_uploder_Domain.py
+++ db_uploder_Domain.py
@@ -2,7 +2,7 @@ import mysql.connector
 def upload(input):
     try:
         connection = mysql.connector.connect(
-            host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",    
+            host="",    
             port=4000,                     
             user="2kRt5fGg8iyL4hs.root",        
             password="6vwhb9RmUNggGdRc",     
