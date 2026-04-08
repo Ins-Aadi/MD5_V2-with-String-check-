aec679d64f40123e70b925d139466c090466f2ba
Aditya Sharma <77775232+Ins-Aadi@users.noreply.github.com>
2026-04-08 21:45:43 +0530

    Update db_connect.py

--- db_connect.py
+++ db_connect.py
@@ -3,10 +3,10 @@ import mysql.connector
 def get_signatures():
     try:
         connection = mysql.connector.connect(
-            host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",    
+            host="",    
             port=4000,                     
-            user="2kRt5fGg8iyL4hs.root",        
-            password="6vwhb9RmUNggGdRc",     
+            user="",        
+            password="",     
             database="antivirus_db",
             ssl_disabled=False  
         )
